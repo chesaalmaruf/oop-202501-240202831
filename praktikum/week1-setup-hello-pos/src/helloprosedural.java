df70bc5daf4086254bb197e13a974e133ef2b546
chesaalmaruf <chesaalmaruf@gmail.com>
2025-10-01 13:56:52 +0700

    helloprosedural.java

---
+++ praktikum/week1-setup-hello-pos/src/helloprosedural.java
@@ -0,0 +1,8 @@
+public class helloprosedural {
+    public static void main(String[] args) {
+        String nim = "240202831";
+        String nama = " chesa salsabil al'ma'ruf" ;
+
+        System.out.println("hello POS world, i'm  " + nama + "nim " + nim);
+}
+}
