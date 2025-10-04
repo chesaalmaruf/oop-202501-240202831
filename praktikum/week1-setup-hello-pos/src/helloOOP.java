c02fa5e41b3c9512ebd4515ff76099a4fa011b5c
chesaalmaruf <chesaalmaruf@gmail.com>
2025-10-04 13:43:53 +0700

    Create helloOOP.java

---
+++ praktikum/week1-setup-hello-pos/src/helloOOP.java
@@ -0,0 +1,12 @@
+import java.util.function.BiConsumer;
+
+public class HelloFunctional {
+    public static void main(String[] args) {
+        // BiConsumer menerima 2 parameter (nama, nim) dan melakukan aksi (print)
+        BiConsumer<String, Integer> printHello = (nama, nim) -> 
+            System.out.println("Hello World, saya " + nama + " dengan NIM: " + nim);
+
+        // Panggil fungsi
+        printHello.accept("Chesa Salsabil Al Ma'ruf", 240202831);
+    }
+}
